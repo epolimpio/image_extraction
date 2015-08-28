8140419786326df69a29696bc71cea5c1f30afe4
epolimpio <epolimpio@gmail.com>
2015-08-28 10:46:31 +0200

    Including rotating data

--- velocity_distribution.py
+++ velocity_distribution.py
@@ -8,13 +8,13 @@ def main(*args):
     Main function to run from outside
     """
 
-    n_frameslst = [31, 40]
-    dtlst = [2.0/3, 0.5]  # time between frames in hours
-    dxlst = [0.67, 0.67]  # XY resolution in microns
-    dzlst = [10, 1]       # Z resolution in microns
+    n_frameslst = [31, 40, 25]
+    dtlst = [2.0/3, 0.5, 0.5]  # time between frames in hours
+    dxlst = [0.67, 0.67, 0.67]  # XY resolution in microns
+    dzlst = [10, 1, 1]       # Z resolution in microns
 
-    datelst = ['2015_8_26_15_56_12','2015_8_27_10_8_37']
-    labels = ['Uninfected', 'Infected non-rotating']
+    datelst = ['2015_8_26_15_56_12','2015_8_27_10_8_37', '2015_8_27_10_28_52']
+    labels = ['Uninfected', 'Infected\nnon-rotating', 'Infected\nrotating']
 
     fig_av_speed = plt.figure(1)
     ax_av_speed = fig_av_speed.add_subplot(1,1,1)
@@ -51,6 +51,14 @@ def main(*args):
         for cell,track_id in enumerate(track_ids):
 
             data = np.array(track.getWholeMovement(track_id))
+
+            if date == '2015_8_27_10_28_52':
+                test = np.logical_and(
+                    np.greater_equal(data[0,:], 4),
+                    np.less(data[0,:], 4 + n_frames)
+                    )
+                data = data[:,test]
+                print(data.shape    )
 
             vx[cell,:] = dx*(data[1,1:n_frames] - data[1,0:n_frames-1])/dt
             vy[cell,:] = dx*(data[2,1:n_frames] - data[2,0:n_frames-1])/dt
